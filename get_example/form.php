8464fe7825f075f7f186ac72b9196b269cdfc2ae
Esraq <esraq26552@gmail.com>
2023-09-01 22:18:49 +0600

    updated code

---
+++ get_example/form.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$firstname=$_GET['firstname'];
+$lastname=$_GET['lastname'];
+$username=$_GET['username'];
+$password=$_GET['password'];
+$email=$_GET['email'];
+
+
+echo "First Name:".$firstname."<br>";
+echo "lastname:".$lastname."<br>";
+echo "Username",$username."<br>";
+
+
+
+
+?>
